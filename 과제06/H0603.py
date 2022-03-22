c44a2572a056c666aee8ecfe86cc5388c3d86d6c
DaYoung Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-03-22 20:31:33 +0900

    Update H0603.py

--- 과제06/H0603.py
+++ 과제06/H0603.py
@@ -1,9 +1,14 @@
 def is_leap_year(y):
-    if y % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
-        return 'True'
+    if y % 4 == 0:
+        if y % 100 == 0:
+            return False
+        elif y % 100 != 0:
+            return True
+        else:
+            return False
     else:
-        return 'False'
-
+        return False
+    
 def main():
     y = int(input('연도: '))
     print('{} 년 윤년 여부 = {}' .format(y, is_leap_year(y)))
