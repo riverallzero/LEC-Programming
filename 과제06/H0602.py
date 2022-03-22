d016302a1dae71bc6fd5add0f1a6b6ff1aeb093f
DaYoung Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-03-22 21:05:57 +0900

    Update H0602.py

--- 과제06/H0602.py
+++ 과제06/H0602.py
@@ -1,10 +1,10 @@
 def range_list(n):
-    return n[0:int(len(n)+1)]
+    return list(range(1, int(n)+1))
 
 def main():
-    num = int(input('숫자: '))
-    n = list(range(1, num+1))
-    print('1-{}까지의 리스트 = {}' .format(num, range_list(n)))
+    n = input('숫자: ')
+    n = n.strip('n=')
+    print('1-{}까지의 리스트 = {}' .format(n, range_list(n)))
 
 if __name__ == '__main__':
     main()
