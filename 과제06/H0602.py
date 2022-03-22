a9f4474a50eeff7a6a61fe51b2b8559a170c5779
DaYoung Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-03-22 19:18:49 +0900

    Update H0602.py

--- 과제06/H0602.py
+++ 과제06/H0602.py
@@ -4,6 +4,7 @@ def range_list(n):
 def main():
     num = int(input('숫자: '))
     n = list(range(1, num+1))
+    
     print('1-{}까지의 리스트 = {}' .format(num, range_list(n)))
 
 if __name__ == '__main__':
