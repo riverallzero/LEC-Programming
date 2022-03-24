f38cd20384985adc9933b6d7d9a5ed8c759d1c80
DaYeong Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-03-24 10:54:36 +0900

    Update H0603.py

--- 과제06/H0603.py
+++ 과제06/H0603.py
@@ -24,7 +24,10 @@ def is_leap_year(y):
 
 def main():
     y = int(input('연도: '))
-    print('윤년 결과는 {}' .format(is_leap_year(y)))
+    if is_leap_year(y):
+        print('윤년이 맞습니다.')
+    else:
+        print('윤년이 아닙니다.')
 
 if __name__ == '__main__':
     main()
