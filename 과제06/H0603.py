6c01b4a7d6f0dfb79a7ffb9730119185607cdfde
DaYeong Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-03-24 10:51:35 +0900

    Update H0603.py

--- 과제06/H0603.py
+++ 과제06/H0603.py
@@ -1,18 +1,30 @@
-def is_leap_year(y):
-    if y % 4 == 0:
-        if y % 100 == 0:
-            return False
-        else:
-            return True
-    else:
-        return False
+# def is_leap_year(y):
+#     if y % 4 == 0:
+#         if y % 100 == 0:
+#             return False
+#         else:
+#             return True
+#     else:
+#         return False
     
+# def main():
+#     y = int(input('연도: '))
+#     if is_leap_year(y):
+#         print('윤년이 맞습니다.')
+#     else:
+#         print('윤년이 아닙니다.')
+        
+# if __name__ == '__main__':
+#     main()
+
+def is_lea_year(y):
+    if y % 4 == 0 and y % 100 != 0:
+        return True
+    return False
+
 def main():
     y = int(input('연도: '))
-    if is_leap_year(y):
-        print('윤년이 맞습니다.')
-    else:
-        print('윤년이 아닙니다.')
-        
+    is_leap_year(y)
+
 if __name__ == '__main__':
     main()
