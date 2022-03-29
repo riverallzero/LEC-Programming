1dec26aa175e858fd0024b6b5bb1ff30c6991323
DaYeong Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-03-29 15:15:38 +0900

    Update H07.py

--- 과제07/H07.py
+++ 과제07/H07.py
@@ -15,10 +15,19 @@ def median(nums):
 
 def main():
     input_text = '5 10 3 4 7'
-    nums = list(map(int, text2list(input_text.split())))
+
+# 파일로 input 불러오기
+#     f = open('파일이름.txt')
+#     input_text = f.readline() #input_text 덮어서 파일 데이터로 불러옴
+#     print('!{}!'. format(input_text))
+
+    nums = list(map(int, text2list(input_text.split())))       
     print('주어진 리스트는', nums)
     print('평균값은 {:.1f}' .format(average(nums)))
     print('중앙값은 {}' .format(median(nums)))
 
 if __name__ == '__main__':
     main()
+    
+    
+    
