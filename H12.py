fdf7e751e7563fb8d7c77f8ba6ddb4c24c66116b
DaYeong Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-04-12 17:13:46 +0900

    Add files via upload

---
+++ H12.py
@@ -0,0 +1,17 @@
+def main():
+    text_list = []
+    while True:
+        text = input('X=? ')
+        try:
+            text = int(text)
+            text_list.append(text)
+        except:
+            pass
+        if text == -1:
+            break
+    print('입력된 값은 {}입니다.'.format(text_list))
+
+if __name__ == '__main__':
+    main()
+
+
