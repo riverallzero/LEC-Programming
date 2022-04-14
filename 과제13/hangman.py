bda092e7678f1d1b41779a3be754fcce6a3ca594
DaYeong Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-04-15 00:25:47 +0900

    Update hangman.py

--- 과제13/hangman.py
+++ 과제13/hangman.py
@@ -1,26 +1,26 @@
-def read_input():
-    return input('알파벳 입력: ')
-
 def main():
-    answer = 'apple'
-    trial = 5
-    answer_list = list(answer)
-    text = list('_____')
-    print(text)
+    answer = list(input('WORD: '))
+    trial = int(input('시도: '))
+    text = list('_'*len(answer))
 
-    for i in range(trial):
-        if read_input() == answer_list[0]:
-            print(text[0].replace('_', read_input()))
-        elif read_input() == answer_list[1]:
-            print(text[1].replace('_', read_input()))
-        elif read_input() == answer_list[2]:
-            print(text[2].replace('_', read_input()))
-        elif read_input() == answer_list[3]:
-            print(text[3].replace('_', read_input()))
-        elif read_input() == answer_list[4]:
-            print(text[4].replace('_', read_input()))
+    while trial > 0:
+        print(text)
+        letter = input('(남은 목숨 {}) 알파벳: ' .format(trial))
+        if letter in answer:
+            for i in range(len(answer)):
+                if letter == answer[i]:
+                    result = text[i] = letter
+                    print(result)
         else:
-            print('정답은 {} 입니다.' .format(answer))
+            trial -= 1
+
+        if '_' not in answer:
+            break
+
+    if trial > 0:
+        print('정답')
+    else:
+        print('정답은 {} 입니다.' .format(answer))
 
 if __name__ == '__main__':
     main()
