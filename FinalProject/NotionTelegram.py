1d53141f7e67bc0eaa0fccd32ec19adaf1bb6f03
KangDayoung <kallzero1008@jbnu.ac.kr>
2022-06-02 17:11:02 +0900

    issue complete

--- FinalProject/NotionTelegram.py
+++ FinalProject/NotionTelegram.py
@@ -2,6 +2,9 @@ import telegram
 from telegram.ext import Updater, MessageHandler
 from telegram.ext import Filters
 import lecfinder as lf
+from selenium import webdriver
+import time
+
 
 token = '5513740361:AAHKlEFGN8ySC7dLpkThZF4-XjnOjus-mSQ'
 id = '5580001250'
@@ -13,19 +16,29 @@ updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 updater.start_polling()
 
+driver = webdriver.Chrome(r'C:\Pych.Projects\Project\chromedriver.exe')
+url = 'https://ieilms.jbnu.ac.kr/'
+driver.get(url)
+time.sleep(2)
 
-# 사용자가 보낸 메세지를 읽어들이고, 답장을 보내줍니다.
-# 아래 함수만 입맛에 맞게 수정해주면 됩니다. 다른 것은 건들 필요없어요.
+driver.find_element_by_xpath('//*[@id="id"]').send_keys('202110065')
+driver.find_element_by_xpath('//*[@id="passwd"]').send_keys('allzero5391.')
+
+loginbtn = driver.find_element_by_xpath('//*[@id="loginform"]/table/tbody/tr[1]/td[2]/input')
+loginbtn.click()
+time.sleep(2)
+
 def handler(update, context):
     user_text = update.message.text  # 사용자가 보낸 메세지를 user_text 변수에 저장합니다.
-    if user_text == "할일":  # 사용자가 보낸 메세지가 "안녕"이면?
+    if user_text == "할일":  # 사용자가 보낸 메세지가 "할일"이면
         bot.send_message(chat_id=id, text='로딩중∙∙∙')
-        report1 = lf.report1_result()
-        report2 = lf.report2_result()
-        video1 = lf.video1_result()
-        video2 = lf.video2_result()
-        quiz1 = lf.quiz1_result()
-        quiz2 = lf.quiz2_result()
+        report1 = lf.report1_result(driver)
+        report2 = lf.report2_result(driver)
+        video1 = lf.video1_result(driver)
+        video2 = lf.video2_result(driver)
+        quiz1 = lf.quiz1_result(driver)
+        quiz2 = lf.quiz2_result(driver)
+
         bot.send_message(chat_id=id, text='{}\n{}'.format(report1, report2))
         bot.send_message(chat_id=id, text='{}\n{}'.format(video1, video2))
         bot.send_message(chat_id=id, text='{}\n{}'.format(quiz1, quiz2))
