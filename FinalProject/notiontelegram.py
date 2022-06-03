aa79bd3bcacc993058ca5254c7698929a4a432aa
DaYeong Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-06-03 13:42:18 +0900

    Update notiontelegram.py

--- FinalProject/notiontelegram.py
+++ FinalProject/notiontelegram.py
@@ -16,23 +16,23 @@ updater = Updater(token=token, use_context=True)
 dispatcher = updater.dispatcher
 updater.start_polling()
 
-driver = webdriver.Chrome(r'C:\Pych.Projects\Project\chromedriver.exe')
-url = 'https://ieilms.jbnu.ac.kr/'
-driver.get(url)
-time.sleep(2)
-
-driver.find_element_by_xpath('//*[@id="id"]').send_keys('202110065')
-driver.find_element_by_xpath('//*[@id="passwd"]').send_keys('allzero5391.')
-
-loginbtn = driver.find_element_by_xpath('//*[@id="loginform"]/table/tbody/tr[1]/td[2]/input')
-loginbtn.click()
-time.sleep(2)
-
 
 def handler(update, context):
     user_text = update.message.text  # 사용자가 보낸 메세지를 user_text 변수에 저장합니다.
     if user_text == "할일":  # 사용자가 보낸 메세지가 "할일"이면
         bot.send_message(chat_id=id, text='로딩중∙∙∙')
+        driver = webdriver.Chrome(r'C:\Pych.Projects\Project\chromedriver.exe')
+        url = 'https://ieilms.jbnu.ac.kr/'
+        driver.get(url)
+        time.sleep(2)
+
+        driver.find_element_by_xpath('//*[@id="id"]').send_keys('202110065')
+        driver.find_element_by_xpath('//*[@id="passwd"]').send_keys('allzero5391.')
+
+        loginbtn = driver.find_element_by_xpath('//*[@id="loginform"]/table/tbody/tr[1]/td[2]/input')
+        loginbtn.click()
+        time.sleep(2)
+
         report1 = lf.report1_result(driver)
         report2 = lf.report2_result(driver)
         video1 = lf.video1_result(driver)
@@ -71,6 +71,9 @@ def handler(update, context):
                 send_telegram_msg(video2)
                 bot.send_message(chat_id=id, text='-> https://ieilms.jbnu.ac.kr/paper/paperList.jsp?group_id=27607')
 
+        logout = driver.find_element_by_xpath('//*[@id="centerTop"]/div[2]/ul/li/div/a[4]')
+        logout.click()
+        time.sleep(2)
 
 def send_telegram_msg(report2):
     if isinstance(report2, tuple):
